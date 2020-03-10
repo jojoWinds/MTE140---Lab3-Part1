861dfbebe5ac25273fc6d2f0d98e3800ff5ae863
Odianosen Okojie <ookojie@edu.uwaterloo.ca>
2020-03-10 14:14:06 -0400

    Finished constructor, get_size, full, max, dequeue functions

--- lab3_starter_code_w2020/lab3_priority_queue.cpp
+++ lab3_starter_code_w2020/lab3_priority_queue.cpp
@@ -5,6 +5,14 @@ using namespace std;
 // PURPOSE: Parametric constructor 
 // initializes heap to an array of (n_capacity + 1) elements
 PriorityQueue::PriorityQueue(unsigned int n_capacity) {
+
+	// double pointer array with capacity of n_capacity + 1
+	heap = new TaskItem*[n_capacity + 1]();
+	
+	//initialise capacity to constructor argument and size to 0
+	capacity = n_capacity;
+	size = 0;
+	
 }
 
 // PURPOSE: Explicit destructor of the class PriorityQueue
@@ -13,6 +21,29 @@ PriorityQueue::~PriorityQueue() {
 
 // PURPOSE: Returns the number of elements in the priority queue
 unsigned int PriorityQueue::get_size() const {
+	
+	//1st case: if heap is empty
+	if(size == 0)
+	{
+		return 0;
+	}
+	//2nd case: list not empty
+	else
+	{
+		// manual check for size 
+		/*
+		int count = 0;
+		while(heap[count])
+		{
+			count++;
+		}
+		
+		return count;
+		*/
+		
+		return size;
+	}
+	
 	return 0;
 }
 
@@ -23,7 +54,12 @@ bool PriorityQueue::empty() const {
 
 // PURPOSE: Returns true if the priority queue is full; false, otherwise
 bool PriorityQueue::full() const {
-	return false;
+	
+	// capacity = size of queue
+	if( capacity == this->get_size())
+		return true
+	else
+		return false;
 }
 
 // PURPOSE: Prints the contents of the priority queue; format not specified
@@ -33,7 +69,19 @@ void PriorityQueue::print() const {
 // PURPOSE: Returns the max element of the priority queue without removing it
 // if the priority queue is empty, it returns (-1, "N/A")
 PriorityQueue::TaskItem PriorityQueue::max() const {
-	return TaskItem(-1, "NULL");
+	
+	//1st case: if the priority queue is empty
+	if(size == 0)
+	{
+		return TaskItem(-1, "NULL"); 
+	}
+	
+	//2nd case: returning what is in the 1st index as based on Max Heap ADT 
+	else
+	{
+		return **heap[1];
+	}
+	//return TaskItem(-1, "NULL");
 }
 
 // PURPOSE: Inserts the given value into the priority queue
@@ -49,5 +97,26 @@ bool PriorityQueue::enqueue( TaskItem val ) {
 // returns true if successful and false otherwise
 // priority queue does not change in capacity
 bool PriorityQueue::dequeue() {
-	return false;
+	
+	//1st case: if list is empty
+	if(size == 0)
+	{
+		return false;
+	}
+	
+	//2nd case: list is populated
+	else
+	{
+		while( size + 1 > 1 && heap[(size+1)/2]->priority < heap[size + 1]->priority)
+		{
+			*TaskItem temp = heap[size + 1];
+			heap[size + 1] = heap[(size + 1)/2];
+			heap[(size + 1)/2] = temp;
+			(size + 1) /= 2;
+		}
+		
+	}
+	
+	size--;
+	return true;
 }
